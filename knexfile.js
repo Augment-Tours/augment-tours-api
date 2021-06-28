f6464ef7053450b157b70640a3ec824ba43d5b91
aymenjelal <aymen2jelal@gmial.com>
2021-06-28 11:47:28 +0300

    ssl true

--- knexfile.js
+++ knexfile.js
@@ -21,8 +21,8 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || {ssl: { rejectUnauthorized: false }},
-    
+    connection: process.env.DATABASE_URL+'?ssl=true',
+    ssl: { rejectUnauthorized: false },
     pool: {
       min: 2,
       max: 10
