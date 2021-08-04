6f4657128e4d0e95f7485ee9e40cbbdadc3ff05b
Amanuel Teferi <aman.teferi.80@gmail.com>
2021-08-04 22:13:17 +0300

    (fix) minor fixes

--- api/controllers/armodelController.js
+++ api/controllers/armodelController.js
@@ -1,52 +1,62 @@
-const express = require('express')
+const express = require("express");
 
-const Armodel =  require('../models/armodel');
+const Armodel = require("../models/armodel");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/:id', async (req, res) => {
-    try{
-        const armodel = await Armodel.getArmodel(req.params.id);
-        res.json(armodel)
-    }
-    catch(err){
-        console.log('error ' + err);
-    }
-    
-  })
+router.get("/:id", async (req, res) => {
+  try {
+    const armodel = await Armodel.getArmodel(req.params.id);
+    res.json(armodel);
+  } catch (err) {
+    console.log("error " + err);
+  }
+});
 
-router.get('/', async (req, res) => {
-    try{
-        const armodels = await Armodel.getAllArModels();
-        res.json(armodels)
-    }
-    catch(err){
-        console.log('error ' + err);
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const armodels = await Armodel.getAllArModels();
+    res.json(armodels);
+  } catch (err) {
+    console.log("error " + err);
+  }
+});
 
-router.get('/:museumId/:floor', async(req,res)=>{
-    try{
-        const armodels = await Armodel.getArmodelsbyMuseumFloor(req.params.museumId,req.params.floor);
-        console.log(armodels);
-        res.json(armodels);
-    }
-    catch(err){
-        console.log('error '+err);
-    }
-})
+router.get("/:museumId/:floor", async (req, res) => {
+  try {
+    const armodels = await Armodel.getArmodelsbyMuseumFloor(
+      req.params.museumId,
+      req.params.floor
+    );
+    console.log(armodels);
+    res.json(armodels);
+  } catch (err) {
+    console.log("error " + err);
+  }
+});
 
-router.post('/', async(req,res)=>{
-    console.log("in create ar model")
-    try{
-        const newArmodel = req.body;
-        console.log("ArModel", newArmodel); 
-        const armodel = await Armodel.addArmodel(newArmodel.name, newArmodel.description, newArmodel.model, newArmodel.x_location,newArmodel.y_location, newArmodel.floor, newArmodel.museums_id, newArmodel.image, newArmodel.x_scale, newArmodel.y_scale, newArmodel.z_scale);
-        res.json(armodel);
-    }
-    catch(err){
-        console.log('error '+err);
-    }
-})
+router.post("/", async (req, res) => {
+  console.log("in create ar model");
+  try {
+    const newArmodel = req.body;
+    console.log("ArModel", newArmodel);
+    const armodel = await Armodel.addArmodel(
+      newArmodel.name,
+      newArmodel.description,
+      newArmodel.model,
+      newArmodel.x_location,
+      newArmodel.y_location,
+      newArmodel.floor,
+      newArmodel.museums_id,
+      newArmodel.image,
+      newArmodel.x_scale,
+      newArmodel.y_scale,
+      newArmodel.z_scale
+    );
+    res.json(armodel);
+  } catch (err) {
+    console.log("error " + err);
+  }
+});
 
-  module.exports = router;
+module.exports = router;
