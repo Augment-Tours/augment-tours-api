460ba5ce5a78439c3b2d49cc0ca49a5907eab95d
Amanuel Teferi <aman.teferi.80@gmail.com>
2021-08-11 15:57:45 +0300

    (fix) fix database url

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      connectionString: process.env.PROD_DB_URL,
       ssl: { rejectUnauthorized: false },
     },
     pool: {
