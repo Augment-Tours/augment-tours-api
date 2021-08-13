1a317c3009dd9d8fb321c756da9086049ede5846
Amanuel Teferi <aman.teferi.80@gmail.com>
2021-08-13 16:36:49 +0300

    (feat) list all target images endpoint

--- api/controllers/targetController.js
+++ api/controllers/targetController.js
@@ -15,6 +15,16 @@ router.get('/:id', async (req, res) => {
     
   })
 
+router.get('/', async (req, res) => {
+    try{
+        const targets = await Target.getAllTargets();
+        res.json(targets)
+    }
+    catch(err){
+        console.log('error ' + err);
+    }
+})
+
 router.get('/museums/:museums_id',async(req,res) => { 
     try{ 
         const targets = await Target.getTargetsByMuseum(req.params.museums_id);
