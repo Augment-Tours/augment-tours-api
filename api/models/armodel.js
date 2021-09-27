42eef2ee384413121736765ecc68a2b170ecb171
aymenjelal <aymen2jelal@gmial.com>
2021-09-27 23:23:15 +0300

    added update for ARModels

--- api/models/armodel.js
+++ api/models/armodel.js
@@ -1,79 +1,124 @@
-const Knex  = require('knex');
-const { Model } = require('objection')
-const Favorite= require('./favorite');
+const Knex = require('knex');
+const { Model } = require('objection');
+const Favorite = require('./favorite');
 const Account = require('./armodel');
-const Museum =  require ('./museum');
-
+const Museum = require('./museum');
 
 class Armodel extends Model {
-    static get tableName () {
-      return 'armodels';
-    }
-  
-    static get relationMappings () {
-      return {
-        museum: {
-          relation: Model.BelongsToOneRelation,
-          modelClass: Museum,
-          join: {
-            from: 'armodels.museums_id',
-            to: 'museums.id'
-          }
+  static get tableName() {
+    return 'armodels';
+  }
+
+  static get relationMappings() {
+    return {
+      museum: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Museum,
+        join: {
+          from: 'armodels.museums_id',
+          to: 'museums.id',
         },
-        favorites: {
-            relation: Model.HasManyRelation,
-            modelClass: Favorite,
-            join: {
-                from: 'armodels.id',
-                to: 'favorites.armodels_id'
-            }
-        }
-      }
-    }
+      },
+      favorites: {
+        relation: Model.HasManyRelation,
+        modelClass: Favorite,
+        join: {
+          from: 'armodels.id',
+          to: 'favorites.armodels_id',
+        },
+      },
+    };
+  }
+}
+
+exports.getArmodel = async (armodelId) => {
+  const armodel = await Armodel.query().findById(armodelId);
+  if (!armodel) {
+    throw new Error('AR Model doesn not exsist');
+  }
+  return armodel;
+};
+
+exports.updateArmodels = async (
+  armodelId,
+  arName,
+  arDescription,
+  arModelurl,
+  arXlocation,
+  arYlocation,
+  arFloor,
+  arMuseumId,
+  arImage,
+  arXscale,
+  arYscale,
+  arZscale
+) => {
+  const armodel = await Armodel.query().findById(armodelId);
+  if (!armodel) {
+    throw new Error('AR Model doesn not exsist');
   }
 
-  exports.getArmodel = async (armodelId)=>{
-    const armodel = await Armodel.query().findById(armodelId);
-    if(!armodel){
-      throw new Error('AR Model doesn not exsist');
-    }
-    return armodel; 
-  };
+  const updatedAR = await Armodel.query().findById(armodelId).patch({
+    name: arName,
+    description: arDescription,
+    model: arModelurl,
+    x_location: arXlocation,
+    y_location: arYlocation,
+    floor: arFloor,
+    museums_id: arMuseumId,
+    image: arImage,
+    x_scale: arXscale,
+    y_scale: arYscale,
+    z_scale: arZscale,
+  });
 
-  exports.getArmodelsbyMuseumFloor = async(museumId, floor) => {
-    console.log(museumId);
-    console.log(floor);
-    const arModels = await Armodel
-    .query()
-    .where('museums_id',museumId)
-    .where('floor',floor);
-    //console.log(arModels);
-    return arModels;
+  return updatedAR;
+};
+
+exports.getArmodelsbyMuseumFloor = async (museumId, floor) => {
+  console.log(museumId);
+  console.log(floor);
+  const arModels = await Armodel.query()
+    .where('museums_id', museumId)
+    .where('floor', floor);
+  //console.log(arModels);
+  return arModels;
+};
+
+exports.getAllArModels = async () => {
+  const arModels = await Armodel.query();
+  return arModels;
+};
+
+exports.addArmodel = async (
+  arName,
+  arDescription,
+  arModelurl,
+  arXlocation,
+  arYlocation,
+  arFloor,
+  arMuseumId,
+  arImage,
+  arXscale,
+  arYscale,
+  arZscale
+) => {
+  const museum = await Museum.getMuseum(arMuseumId);
+  if (!museum) {
+    throw new Error('Museum does not exsist');
   }
-
-  exports.getAllArModels = async ()=>{
-    const arModels = await Armodel.query();
-    return arModels;
-  };
-
-  exports.addArmodel = async (arName,arDescription ,arModelurl,arXlocation,arYlocation,arFloor,arMuseumId,arImage,arXscale,arYscale,arZscale)=>{
-    const museum = await Museum.getMuseum(arMuseumId);
-    if(!museum){
-      throw new Error('Museum does not exsist');
-    }
-    const armodel = await Armodel.query().insert({
-      name: arName,
-      description: arDescription,
-      model: arModelurl,
-      x_location: arXlocation,
-      y_location: arYlocation,
-      floor:arFloor,
-      museums_id: arMuseumId,
-      image: arImage,
-      x_scale: arXscale,
-      y_scale: arYscale,
-      z_scale: arZscale    
-    });
-    return armodel;
-  }
-
+  const armodel = await Armodel.query().insert({
+    name: arName,
+    description: arDescription,
+    model: arModelurl,
+    x_location: arXlocation,
+    y_location: arYlocation,
+    floor: arFloor,
+    museums_id: arMuseumId,
+    image: arImage,
+    x_scale: arXscale,
+    y_scale: arYscale,
+    z_scale: arZscale,
+  });
+  return armodel;
+};
